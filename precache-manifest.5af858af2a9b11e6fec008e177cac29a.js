5bf291bd7107a61ed59aec43ee75ba387b1c6c6d
Юлия Михайлова <jullidu@gmail.com>
2019-11-07 12:10:04 +0800

    Updates

---
+++ precache-manifest.5af858af2a9b11e6fec008e177cac29a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de31e704d214e9b3bf328cf2d121ad37",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "29d7bf758de9125bca28",
+    "url": "/robofriends/static/css/main.36669c28.chunk.css"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/robofriends/static/js/2.d75c0efc.chunk.js"
+  },
+  {
+    "revision": "29d7bf758de9125bca28",
+    "url": "/robofriends/static/js/main.65a688aa.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
